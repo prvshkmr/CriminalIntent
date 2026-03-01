2cd1f9f9c6155240efa6a41917e1f0f7d397c3f6
prvshkmr <parvesh427620@gmail.com>
2026-03-01 17:25:24 +0530

    Verifying the .kt extension logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.fragment.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
